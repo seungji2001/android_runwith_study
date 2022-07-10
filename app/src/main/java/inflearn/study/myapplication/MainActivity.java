b3e93550f21f0884efae4c4090a195c4af25ac40
Username <lisa2009343123456@gmail.com>
2022-07-10 23:33:53 +0900

    5th commit - login

--- app/src/main/java/inflearn/study/myapplication/MainActivity.java
+++ app/src/main/java/inflearn/study/myapplication/MainActivity.java
@@ -7,11 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText etId;
     private Button btnLogin;
+    private Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,13 +30,41 @@ public class MainActivity extends AppCompatActivity {
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //사용자가 입력한 id 와 pw 가져오기
-                String id = etId.getText().toString();
-                //String pw = etPw.getText().toString();
+            public void onClick(View view) {
+                String userID = etId.getText().toString();
 
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                intent.putExtra("id",id);
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if(success){//회원등록에 성공한 경우
+                                String userID = jsonObject.getString("userID");
+                                Toast.makeText(getApplicationContext(),"로그인에 성공했습니다",Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                                intent.putExtra("id",userID);
+                                startActivity(intent);
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(),"로그인에 실패했습니다",Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(userID,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                queue.add(loginRequest);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
                 startActivity(intent);
             }
         });
@@ -36,5 +73,6 @@ public class MainActivity extends AppCompatActivity {
     private void setLayout() {
         etId = (EditText) findViewById(R.id.etId);
         btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
     }
 }
