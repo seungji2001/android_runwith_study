894f576e73f1fe22b7d6160c4cd0af8d6a74497a
Username <lisa2009343123456@gmail.com>
2022-07-19 17:35:02 +0900

    내정보추가하기, 메세지 주고받는 기능 xml레이아웃 완성하기

--- app/src/main/java/inflearn/study/myapplication/MeActivity.java
+++ app/src/main/java/inflearn/study/myapplication/MeActivity.java
@@ -10,13 +10,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MeActivity extends Fragment {
 
     private TextView textView;
     private View view;
-
+    private Button button_helper;
     public MeActivity(){
 
     }
@@ -26,6 +27,7 @@ public class MeActivity extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_me,container,false);
         textView = view.findViewById(R.id.id);
+        button_helper = view.findViewById(R.id.button_helper);
 
         Bundle bundle = getArguments();
 
